3afdc892b10eb2fc8fe3c51d0ee3c80517fbfb0a
Tawsif Rahman Shopnil <shopnil@baiust.edu.bd>
2023-09-24 18:28:51 +0600

    admin_dashboard_style_add

--- volcanosx/index.php
+++ volcanosx/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>Log In | Adminto - Responsive Admin Dashboard Template</title>
+        <title>Log In | Swinroll Tech Admin Dashboard </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
         <meta content="Coderthemes" name="author" />
