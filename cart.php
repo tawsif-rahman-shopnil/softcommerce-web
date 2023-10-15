d2c0fc356df6aa52c27a7d29cc344e2a23d117c8
Tawsif Rahman Shopnil <shopnil@baiust.edu.bd>
2023-10-16 02:47:23 +0600

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,28 @@
+<?php
+$page = 'cart'; // Set the active page
+echo "Current page: $page"; // Debugging line
+include 'header.php';
+?>
+
+
+  <div class="container" style="margin-top: 90px;" >
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-content">
+
+          <!-- ***** Buttons Area Start ***** -->
+          <div class="buttons-container">
+            <div class="heading-section">
+                  <h4>Sign-up or Login to Continue</h4>      
+                <div class="button">
+                    <a href="signup.php">Sign-up</a></br>
+                    <a href="login.php">Login</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        <!-- ***** Buttons Area End ***** -->
+        </div>
+ </br>
+  <?php require 'footer.php'; ?>
